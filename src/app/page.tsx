e408dde58fa86ebef8dcb0059967d500e2a65846
AbSrDUO1 <163032126+AbSrDUO1@users.noreply.github.com>
2025-10-17 18:56:43 +0530

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { ThemeProvider } from '@/providers/ThemeProvider';
 import NavbarLayoutFloatingOverlay from '@/components/navbar/NavbarLayoutFloatingOverlay/NavbarLayoutFloatingOverlay';
@@ -8,8 +8,6 @@ import FeatureCardTwo from '@/components/sections/feature/FeatureCardTwo';
 import ProductCardOne from '@/components/sections/product/ProductCardOne';
 import ContactSplit from '@/components/sections/contact/ContactSplit';
 import FooterLogoEmphasis from '@/components/sections/footer/FooterLogoEmphasis';
-
-const assetMap = [{"id":"hero-image","url":"https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/pizzeria-interior-1760707404783-62d70cc9.jpg","alt":"pizzeria interior"},{"id":"feature-pizza","url":"https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/delicious-pizza-1760707409914-79ea8fda.jpg","alt":"delicious pizza"},{"id":"about-image","url":"https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/pizzeria-team-1760707412823-67155408.jpg","alt":"pizzeria team"},{"id":"contact-image","url":"https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/pizzeria-exterior-1760707415404-c27a2240.jpg","alt":"pizzeria exterior"}];
 
 export default function Home() {
   return (
@@ -36,7 +34,7 @@ export default function Home() {
           <HeroBillboard
             title="Welcome to Pizzeria"
             description="Experience the finest pizzas in town."
-            imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/pizzeria-interior-1760707404783-62d70cc9.jpg"
+            imageSrc="asset://hero-image"
             buttons={[
               { text: "See Our Menu", href: "menu" },
               { text: "About Us", href: "about" }
@@ -84,19 +82,19 @@ export default function Home() {
                 id: "1",
                 name: "Margherita",
                 price: "$12.99",
-                imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/delicious-pizza-1760707409914-79ea8fda.jpg"
+                imageSrc: "asset://feature-pizza"
               },
               {
                 id: "2",
                 name: "Pepperoni",
                 price: "$14.99",
-                imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/delicious-pizza-1760707409914-79ea8fda.jpg"
+                imageSrc: "asset://feature-pizza"
               },
               {
                 id: "3",
                 name: "Veggie",
                 price: "$13.99",
-                imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/delicious-pizza-1760707409914-79ea8fda.jpg"
+                imageSrc: "asset://feature-pizza"
               }
             ]}
           />
@@ -108,7 +106,7 @@ export default function Home() {
             tag="Contact Us"
             title="Get in Touch"
             description="We'd love to hear from you! Visit us or send a message."
-            imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_346q1PwyWBLFgxn5R5gWYFVRO0Y/tmp/pizzeria-exterior-1760707415404-c27a2240.jpg"
+            imageSrc="asset://contact-image"
           />
         </div>
       </div>
